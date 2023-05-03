68ea77916eac29ec996a574e7275fb19f84c0c46
bletchley-park <rushdeepd@gmail.com>
2023-05-04 02:55:10 +0530

    Student Model added

--- FoodDeliveryWebApp/Students/models.py
+++ FoodDeliveryWebApp/Students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    studentID = models.CharField(max_length=8)
+    studentName = models.CharField(max_length=100)
+    hostelID = models.ForeignKey(User, on_delete=models.CASCADE)
+    accountNumber = models.CharField(max_length=10)
+    userName = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
