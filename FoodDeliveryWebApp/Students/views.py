84a3fb287f3f7d45bb681364b3130007fc3ac996
Aditya Harish <adityaharish99@gmail.com>
2023-05-21 11:30:20 +0530

    Display Total Order Price

--- FoodDeliveryWebApp/Students/views.py
+++ FoodDeliveryWebApp/Students/views.py
@@ -15,10 +15,16 @@ class StudentOrdersView(View):
     def get(self, request, student_id):
         student = StudentUser.objects.get(id=student_id)
         orders = student.ordermodel_set.all()
+        total_price = 0
+        for order in orders:
+            items = order.items.all()
+            order_price = sum(item.price for item in items)
+            total_price += order_price
 
         context = {
             'student': student,
             'orders': orders,
+            'totalprice': total_price
         }
         return render(request, 'Students/orders.html', context)
 
